e8dd8c04e2c0d2fddd6db208ba934b8b61a26922
Shayan Ahmed <shayanlost1@gmail.com>
2023-06-02 16:20:41 +0500

    build fix

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -58,23 +58,23 @@ const TodoList = () => {
     <>
     <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
 
-      {data.map((item) => {
-        return (
-          <div className="p-6 bg-white rounded-lg shadow-lg">
-          <h2 className="mb-4 text-xl font-bold">Card Title</h2>
-          <p className="mb-4 text-gray-600">{item.task}</p>
-          <div className="flex items-center justify-between">
-           
-           {/*edit button implementation */}
-           <PopupScreen refresh={refreshList} comptask={item.task} compid={item.id}/>
+    {data.map((item) => {
+  return (
+    <div className="p-6 bg-white rounded-lg shadow-lg" key={item.id}>
+      <h2 className="mb-4 text-xl font-bold">Card Title</h2>
+      <p className="mb-4 text-gray-600">{item.task}</p>
+      <div className="flex items-center justify-between">
+        {/* edit button implementation */}
+        <PopupScreen refresh={refreshList} comptask={item.task} compid={item.id} />
 
-          <button onClick={() => handleDel(item.id, refreshList)}>
-          <Image src={delicon} alt="delete icon"/>
-          </button>
-          </div>
-        </div>
-        );
-      })}
+        <button onClick={() => handleDel(item.id, refreshList)}>
+          <Image src={delicon} alt="delete icon" />
+        </button>
+      </div>
+    </div>
+  );
+})}
+
           </div>
      <AddTodos refresh={refreshList}/>
     </>
