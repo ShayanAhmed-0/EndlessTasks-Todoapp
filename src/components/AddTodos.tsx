63f28face1a869d127d15e92621d87918e303b19
Shayan Ahmed <shayanlost1@gmail.com>
2023-06-07 03:19:27 +0500

    todoapp

--- src/components/AddTodos.tsx
+++ src/components/AddTodos.tsx
@@ -1,51 +1,56 @@
 import { NewTodo, Todo } from '@/lib/drizzle';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 
-interface refunc{
-  refresh:()=>void
+interface Refunc {
+  refresh: () => void;
 }
 
+const AddTodos = ({ refresh }: Refunc) => {
+  const [task, setTask] = useState<string>('');
 
-
-const AddTodos = ({refresh}:refunc) => {
-    const [task, setTask] = useState<NewTodo | null>(null);
-    const AddTodo = async () => {
-      console.log(refresh)
-        try {
-          if (task) {
-            const res = await fetch("/api/Todo", {
-              method: "POST",
-              body: JSON.stringify({
-                title:task.task,
-                task: task.task
-              }),
-            });
-            await refresh(); 
-          }
-        } catch (error) {
-          console.log("Error AddTODO");
-        }
-      };
+  const addTodo = async () => {
+    try {
+      if (task) {
+        const res = await fetch('/api/Todo', {
+          method: 'POST',
+          body: JSON.stringify({
+            title: task,
+            task: task
+          }),
+        });
+        await refresh();
+        setTask('');
+      }
+    } catch (error) {
+      console.log('Error AddTODO');
+    }
+  };
 
   return (
     <div>
-       <div className="flex justify-center">
+      <div className="flex justify-center">
         <form className="w-4/5">
           <input
-            
-            
+            value={task}
+            onChange={(e) => {
+              setTask(e.target.value);
+            }}
             className="w-full border border-blue-300 rounded focus:outline-blue-400"
             type="text"
           />
           <div className="flex justify-center">
-          <button type="button" className="p-2 pl-4 pr-4 m-2 bg-blue-400 rounded" onClick={AddTodo}>
-            Submit
-          </button>
+            <button
+              type="button"
+              className="p-2 pl-4 pr-4 m-2 bg-blue-400 rounded"
+              onClick={addTodo}
+            >
+              Submit
+            </button>
           </div>
         </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default AddTodos
+export default AddTodos;
